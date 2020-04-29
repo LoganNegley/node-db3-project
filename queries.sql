f8bb222a570b6c6dfb0a88d9227f066317dd9ef4
Logan Negley <logannegley@gmail.com>
2020-04-28 20:17:56 -0500

    all queries except last one

--- queries.sql
+++ queries.sql
@@ -6,7 +6,19 @@ FROM [Product]
 JOIN [Category] 
 ON [Product].CategoryId = [Category].Id
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-
+SELECT Id, CompanyName
+FROM [Order]
+JOIN [Shipper]
+ON [Order].ShipVia = [Shipper].Id
+WHERE [Order].OrderDate < '2012-08-09'
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+SELECT ProductName, Quantity 
+FROM [OrderDetail]
+JOIN [Product]
+ON [OrderDetail].ProductId = [Product].ID
+WHERE OrderId = 10251 
+ORDER By ProductName DESC 
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT OrderId, CompanyName, LastName
+FROM []
